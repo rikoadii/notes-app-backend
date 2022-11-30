ce753bb49d9be4b13613e6a3431ca6bb225c9370
rikoadii <radi60372@gmail.com>
2022-11-30 10:37:35 +0800

    initial-commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNoteHandler,
+  getAllNoteHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
